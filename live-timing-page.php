13c9de5b77858e12e35f372350a6418197885c35
formulapaddock90-hue <formulapaddock90@gmail.com>
2026-08-21 12:53:20 +0200

    Fix live timing cache version

--- live-timing-page.php
+++ live-timing-page.php
@@ -16,7 +16,7 @@ get_header();
 <div id="primary" class="content-area w-100 p-0 m-0">
     <main id="main" class="site-main w-100 p-0 m-0" role="main">
         <div class="undercut-iframe-wrapper" style="width: 100%; min-height: 1350px; background: #0b0f19; margin: 0; padding: 0;">
-            <iframe src="https://www.formulapaddock.it/live.html?v=14.0" style="width: 100%; height: 1350px; border: none; overflow: hidden;" allow="autoplay; fullscreen" title="F1 Live Timing Telemetria Direct Stream"></iframe>
+            <iframe src="https://www.formulapaddock.it/live.html?v=15.0" style="width: 100%; height: 1350px; border: none; overflow: hidden;" allow="autoplay; fullscreen" title="F1 Live Timing Telemetria Direct Stream"></iframe>
         </div>
     </main>
 </div>
